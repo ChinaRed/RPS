56f9587e84934c20f1c2cabfb4cb4cfb3da3f0fa
Jon <jonathan.tan83@gmail.com>
2015-01-26 20:11:02 -1000

    images, computer input, submit button

--- rps.js
+++ rps.js
@@ -1,12 +1,43 @@
-var myArray = ["R","P","S"];
-var compRandom = myArray[Math.floor(Math.random() * myArray.length)];
+var rock = document.getElementById('rock');
+rock.style.visibility= "hidden";
+var paper = document.getElementById('paper');
+paper.style.visibility= "hidden";
+var scissor = document.getElementById('scissor');
+scissor.style.visibility= "hidden";
 
-function winner (user){
+var result = document.getElementById("result");
+
+var myArray = ["R","P","S"];
+
+function winner (submit){
+  var user = document.getElementById('userInput').value.toUpperCase();
+  var compRandom = myArray[Math.floor(Math.random() * myArray.length)];
+  console.log(user);
+
   if ( user === compRandom ){
-    alert("Tie Bitchessssssss!");
-    } else if (user === 'R' && compRandom === 'S' || user === 'S' && compRandom === 'P' || user === 'P' && compRandom === 'R'){
-      alert("You Winssssssss!");
-      } else {
-        alert("You Lose Suckerrrrrrr!");
-      }
+    //alert("Tie Bitchessssssss!");
+    result.innerHTML = "Tie Bitchessssssss";
+    
+  } else if (user === 'ROCK' && compRandom === 'S' || user === 'SCISSOR' && compRandom === 'P' || user === 'PAPERf' && compRandom === 'R'){
+    result.innerHTML = "You're Winnerrrrrrrr";
+      
+  } else {
+    result.innerHTML = "You Lose Suckaaaaaaaa";
+        
+  }
+  if (compRandom === 'R'){
+    paper.style.visibility= "hidden";
+    rock.style.visibility= "visible";
+    scissor.style.visibility= "hidden";
+  } else if (compRandom === 'P'){
+    paper.style.visibility= "visible";
+    rock.style.visibility= "hidden";
+    scissor.style.visibility= "hidden";
+  } else {
+    paper.style.visibility= "hidden";
+    rock.style.visibility= "hidden";
+    scissor.style.visibility= "visible";
+  }
+  console.log(compRandom);
 }
+

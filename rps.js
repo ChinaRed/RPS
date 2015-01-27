6ea28cffb81f985202db9ca5bf1d0c70bea163c0
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-01-27 09:56:06 -1000

    adds images for user's choice

--- rps.js
+++ rps.js
@@ -6,10 +6,20 @@ paper.style.visibility= "hidden";
 var scissor = document.getElementById('scissor');
 scissor.style.visibility= "hidden";
 
+
+var rock2 = document.getElementById('rock2');
+rock2.style.visibility= "visible";
+var paper2 = document.getElementById('paper2');
+paper2.style.visibility= "visible";
+var scissor2 = document.getElementById('scissor2');
+scissor2.style.visibility= "visible";
+
+
+
 var result = document.getElementById("result");
 
 
-var myArray = ["R","P","S"];
+var myArray = ["ROCK","PAPER","SCISSOR"];
 
 function winner (submit){
 
@@ -22,18 +32,32 @@ function winner (submit){
     //alert("Tie Bitchessssssss!");
     result.innerHTML = "Tie Bitchessssssss";
     
-  } else if (user === 'ROCK' && compRandom === 'S' || user === 'SCISSOR' && compRandom === 'P' || user === 'PAPERf' && compRandom === 'R'){
+  } else if (user === 'ROCK' && compRandom === 'SCISSOR' || user === 'SCISSOR' && compRandom === 'PAPER' || user === 'PAPER' && compRandom === 'ROCK'){
     result.innerHTML = "You're Winnerrrrrrrr";
       
   } else {
     result.innerHTML = "You Lose Suckaaaaaaaa";
         
   }
-  if (compRandom === 'R'){
+  if (user === 'ROCK'){
+    paper2.style.visibility= "hidden";
+    rock2.style.visibility="visible";
+    scissor2.style.visibility= "hidden";
+  } else if (user === 'PAPER'){
+    paper2.style.visibility= "visible";
+    rock2.style.visibility="hidden";
+    scissor2.style.visibility= "hidden";
+  } else {
+    paper2.style.visibility= "hidden";
+    rock2.style.visibility="hidden";
+    scissor2.style.visibility= "visible";
+  }
+  
+  if (compRandom === 'ROCK'){
     paper.style.visibility= "hidden";
     rock.style.visibility= "visible";
     scissor.style.visibility= "hidden";
-  } else if (compRandom === 'P'){
+  } else if (compRandom === 'PAPER'){
     paper.style.visibility= "visible";
     rock.style.visibility= "hidden";
     scissor.style.visibility= "hidden";

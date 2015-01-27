2beb36d5bd98d97b1a851603d75b522e8411413c
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-01-26 19:45:26 -1000

    links html to js

--- rps.js
+++ rps.js
@@ -1,12 +1,36 @@
-var myArray = ["R","P","S"];
-var compRandom = myArray[Math.floor(Math.random() * myArray.length)];
+document.getElementById('rock').style.visibility= "hidden";
+document.getElementById('paper').style.visibility= "hidden";
+document.getElementById('scissor').style.visibility= "hidden";
 
-function winner (user){
+var myArray = ["R","P","S"];
+
+function winner (submit){
+  var user = document.getElementById('submit').value.toUpperCase();
+  var compRandom = myArray[Math.floor(Math.random() * myArray.length)];
+  console.log(user);
+
   if ( user === compRandom ){
     alert("Tie Bitchessssssss!");
-    } else if (user === 'R' && compRandom === 'S' || user === 'S' && compRandom === 'P' || user === 'P' && compRandom === 'R'){
-      alert("You Winssssssss!");
-      } else {
-        alert("You Lose Suckerrrrrrr!");
-      }
+    
+  } else if (user === 'ROCK' && compRandom === 'S' || user === 'SCISSOR' && compRandom === 'P' || user === 'PAPERf' && compRandom === 'R'){
+    alert("You Winssssssss!");
+      
+  } else {
+    alert("You Lose Suckerrrrrrr!");
+        
+  }
+  if (compRandom === 'R'){
+    document.getElementById('paper').style.visibility= "hidden";
+    document.getElementById('rock').style.visibility= "visible";
+    document.getElementById('scissor').style.visibility= "hidden";
+  } else if (compRandom === 'P'){
+    document.getElementById('paper').style.visibility= "visible";
+    document.getElementById('rock').style.visibility= "hidden";
+    document.getElementById('scissor').style.visibility= "hidden";
+  } else {
+    document.getElementById('paper').style.visibility= "hidden";
+    document.getElementById('rock').style.visibility= "hidden";
+    document.getElementById('scissor').style.visibility= "visible";
+  }
+  console.log(compRandom);
 }
